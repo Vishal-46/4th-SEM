8a18b562e8a67025b1705a052ae7b3bfc7c790cb
Vishal-46 <vishalmurugan006@gmail.com>
2025-02-28 05:03:21 +0530

    Updated ID3

---
+++ ML-AD3311/ID3.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder
+data = pd.DataFrame([
+    ['Sunny', 'Hot', 'High', 'Weak', 'No'],
+    ['Sunny', 'Hot', 'High', 'Strong', 'No'],
+    ['Overcast', 'Hot', 'High', 'Weak', 'Yes'],
+    ['Rain', 'Mild', 'High', 'Weak', 'Yes'],
+    ['Rain', 'Cool', 'Normal', 'Weak', 'Yes'],
+    ['Rain', 'Cool', 'Normal', 'Strong', 'No'],
+    ['Overcast', 'Cool', 'Normal', 'Strong', 'Yes'],
+    ['Sunny', 'Mild', 'High', 'Weak', 'No'],
+    ['Sunny', 'Cool', 'Normal', 'Weak', 'Yes'],
+    ['Rain', 'Mild', 'Normal', 'Weak', 'Yes'],
+    ['Sunny', 'Mild', 'Normal', 'Strong', 'Yes'],
+    ['Overcast', 'Mild', 'High', 'Strong', 'Yes'],
+    ['Overcast', 'Hot', 'Normal', 'Weak', 'Yes'],
+    ['Rain', 'Mild', 'High', 'Strong', 'No']
+], columns=['Outlook', 'Temperature', 'Humidity', 'Wind', 'PlayTennis'])
+label_encoders = {}  
+for col in data.columns:
+    le = LabelEncoder()
+    data[col] = le.fit_transform(data[col])
+    label_encoders[col] = le  
+X = data.drop(columns=['PlayTennis'])
+y = data['PlayTennis']
+model = DecisionTreeClassifier(criterion='entropy')
+model.fit(X, y)
+new_sample = pd.DataFrame([['Sunny', 'Cool', 'High', 'Strong']], 
+                          columns=['Outlook', 'Temperature', 'Humidity', 'Wind'])
+for col in new_sample.columns:
+    new_sample[col] = label_encoders[col].transform(new_sample[col])
+prediction = model.predict(new_sample)
+predicted_label = label_encoders['PlayTennis'].inverse_transform(prediction)
+print("Predicted class for new sample:", predicted_label[0])
