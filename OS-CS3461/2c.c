94298d7b5923953983dd799f512efd42914a79c3
Vishal-46 <vishalmurugan006@gmail.com>
2025-03-26 04:39:15 +0530

    OS

--- OS-CS3461/2c.c
+++ OS-CS3461/2c.c
@@ -3,31 +3,26 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #define SIZE 1024
-
 main(int argc, char *argv[])
 {
     int src, dst, nread;
-    char buf[SIZE];
-    
+    char buf[SIZE]; 
     if (argc != 3)
     {
         printf("Usage: gcc copy.c -o copy\n");
         printf("Usage: ./copy <filename> <newfile>\n");
         exit(-1);
     }
-    
     if ((src = open(argv[1], O_RDONLY)) == -1)
     {
         perror(argv[1]);
         exit(-1);
-    }
-    
+    }  
     if ((dst = creat(argv[2], 0644)) == -1)
     {
-        perror(argv[2]);  // Fixed to show destination filename in error
+        perror(argv[2]);  
         exit(-1);
-    }
-    
+    } 
     while ((nread = read(src, buf, SIZE)) > 0)
     {
         if (write(dst, buf, nread) == -1)
@@ -36,7 +31,6 @@ main(int argc, char *argv[])
             exit(-1);
         }
     }
-    
     close(src);
     close(dst);
 }
