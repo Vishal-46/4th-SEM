c84258112494d6ee9f5c9b93e3912fbb082f2494
Vishal-46 <vishalmurugan006@gmail.com>
2025-04-01 04:44:56 +0530

    Dataset

--- ML-AD3311/naive.py
+++ ML-AD3311/naive.py
@@ -90,7 +90,7 @@ def get_accuracy(test_set, predictions):
     return (correct/float(len(test_set))) * 100.0
 
 def main():
-    filename = 'D:\Lab EXPs'
+    filename = 'D:\Lab EXPs\diabetes.csv'
     split_ratio = 0.67
     dataset = load_csv(filename)
     training_set, test_set = split_dataset(dataset, split_ratio)
